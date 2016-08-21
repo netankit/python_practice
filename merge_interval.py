72b39488e51dce6a50248790819fd0b6f310dba0
Ankit Bahuguna <netankit@gmail.com>
2016-08-21 16:47:53 +0200

    Added solution to merge_interval problem

--- merge_interval.py
+++ merge_interval.py
@@ -6,26 +6,27 @@ Given [1,3],[2,6],[8,10],[15,18],
 return [1,6],[8,10],[15,18]
 """
 
-class Interval(object):
-	def __init__(self):
-		self.start = 0
-		self.end = 0
-
-	def __init__(self, s, e):
-		self.start = s
-		self.end = e
-
-
-	
-
-
-
-
 def merge_overlapping_intervals(input):
+	start = 0
+	end = 1
+	result = []
+	if (input == None or len(input)<=1):
+		return input
 
+	# SORT intervals using self custom defined comparator key, inplace
+	input.sort(key=lambda x:x[start])
+	prev = input[start]
+	for i in xrange(1, len(input)):
+		curr = input[i]
+		if prev[end] >= curr[start]:
+			merged = [prev[start], max(prev[end], curr[end])]
+			prev = merged
+		else:
+			result.append(prev)
+			prev = curr
 
-
-
+	result.append(prev)
+	return result
 
 
 
