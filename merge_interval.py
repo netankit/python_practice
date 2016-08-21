e69a9c3a31025d967dfd5976f0550347ccadd236
Ankit Bahuguna <netankit@gmail.com>
2016-08-21 16:20:41 +0200

    testing git commit

--- merge_interval.py
+++ merge_interval.py
@@ -5,3 +5,29 @@ For example,
 Given [1,3],[2,6],[8,10],[15,18],
 return [1,6],[8,10],[15,18]
 """
+
+class Interval(object):
+	def __init__(self):
+		self.start = 0
+		self.end = 0
+
+	def __init__(self, s, e):
+		self.start = s
+		self.end = e
+
+
+	
+
+
+
+
+def merge_overlapping_intervals(input):
+
+
+
+
+
+
+
+input_array = [[1,3],[2,6],[8,10],[15,18]]
+print merge_overlapping_intervals(input_array)
